95081c00c187dad078623162d41be6640c629e35
scottmcpherson <scott@raisal.com>
2018-07-24 13:52:05 -0700

    Remove style tags from forms

--- client/src/routes/ForgotPassword/ForgotPassword.js
+++ client/src/routes/ForgotPassword/ForgotPassword.js
@@ -34,18 +34,6 @@ const renderTextField = ({ input, meta: { touched, error }, ...custom }) => (
 const ForgotPasswordForm = ({ handleSubmit, pristine, submitting }) => {
   return (
     <div>
-      {/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-      <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
       <Grid
         textAlign="center"
         style={{ height: '100%' }}
--- client/src/routes/SignUp/SignUp.js
+++ client/src/routes/SignUp/SignUp.js
@@ -36,18 +36,6 @@ const SignUpForm = ({ handleSubmit, pristine, submitting, formErrors }) => {
   const isFormErrors = !!(Array.isArray(formErrors) && formErrors.length)
   return (
     <div>
-      {/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-      <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
       <Grid
         textAlign="center"
         style={{ height: '100%' }}
